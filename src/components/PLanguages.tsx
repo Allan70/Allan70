84fc6d571cffc2628e3b1a5289f2613bf69060dc
Allan70 <aabere70@gmail.com>
2025-10-02 01:26:27 +0300

    FIX: Adding UI changes

--- src/components/PLanguages.tsx
+++ src/components/PLanguages.tsx
@@ -1,7 +1,7 @@
 import cpp from "../images/svgs/c-plus-plus-svgrepo-com.svg"
 import cs from "../images/svgs/c-sharp-svgrepo-com.svg"
 import go from "../images/svgs/go-svgrepo-com.svg"
-import html from "../images/svgs/html-5.svg"
+// import html from "../images/svgs/html-5.svg"
 import java from "../images/svgs/java-svgrepo-com (2).svg"
 import js from "../images/svgs/javascript.svg"
 import node_dark from "../images/svgs/nodejs-icon-svgrepo-com.svg"
@@ -10,9 +10,9 @@ import node_dark from "../images/svgs/nodejs-icon-svgrepo-com.svg"
 export default function PLanguages() {
   return (
     <div className="code-badges">
-        <div className="html badge-itm">
+        {/* <div className="html badge-itm">
           <img src={html} alt="html" className="badge-logo"/>
-        </div>
+        </div> */}
         <div className="node badge-itm">
           <img src={node_dark} alt="node" className="badge-logo"/>
         </div>
